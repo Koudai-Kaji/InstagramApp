7b2f92a1cbda8822ec6cf2ff78654db1d5f9b73d
鍛治巧大 <kajikoudai@MacBook-Air.local>
2020-05-19 08:15:05 +0900

    Add users_controller

---
+++ test/integration/homepage_layout_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class HomepageLayoutTest < ActionDispatch::IntegrationTest
+
+  test "layouts links" do
+    get root_path
+    assert_template "static_pages/home"
+    assert_select 'a[href=?]', signup_path
+  end
+
+end
