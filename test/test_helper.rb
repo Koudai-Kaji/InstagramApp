8a0cbf4d9973400672367fee86bf788960f030fa
鍛治巧大 <kajikoudai@MacBook-Air.local>
2020-05-22 11:17:47 +0900

    Add login helper in test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,15 +3,27 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   include ApplicationHelper
 
-  # Add more helper methods to be used by all tests here...
 
   #ログインしていればtrue,それ以外はfalse
   def is_logged_in?
     !session[:user_id].nil?
+  end
+
+  #ログイン
+  def log_in_as(user)
+    session[:user_id] = user.id 
+  end
+
+end
+
+class ActionDispatch::IntegrationTest
+
+  def log_in_as(user, password: "password")
+    post login_path, params: {session: {email: user.email,
+                                        password: password}}
   end
 
 end
