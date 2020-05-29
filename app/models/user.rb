080bae76f276efd7afb76f983eee4fe1dd681c2d
鍛治巧大 <kajikoudai@MacBook-Air.local>
2020-05-29 14:34:39 +0900

    Add relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
-
   has_many :user_images, dependent: :destroy
+  has_many :active_relationships,   class_name:  "Relationship",
+                                    foreign_key: "follower_id",
+                                    dependent:   :destroy
+  has_many :passive_relationships,  class_name:  "Relationship",
+                                    foreign_key: "followed_id",
+                                    dependent:   :destroy
+  has_many :following, through: :active_relationships,  source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower 
   before_save {email.downcase!}
   validates :name, {presence: true, length: {maximum: 50}}
   VALLID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -19,5 +26,22 @@ class User < ApplicationRecord
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+
+  #ユーザーをフォローする
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  #ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  #現在のユーザーがフォローしているか確認
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
+
 
 end
