f247b5050e54715819f7e28a11e8dc9688861b6b
鍛治巧大 <kajikoudai@MacBook-Air.local>
2020-06-02 08:21:10 +0900

    modfy comment form in home-page because of an erorr

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
 
   def home
     @feed_items = current_user.feed.paginate(page: params[:page]) if logged_in?
-    @comment    = current_user.comments.build
+    @comment    = current_user.comments.build if logged_in?
   end
   
 end
